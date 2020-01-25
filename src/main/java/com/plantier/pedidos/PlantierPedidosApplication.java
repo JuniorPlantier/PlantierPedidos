6ac36e63fa7f56576e7e641ef72be70ff5ef9893
Carlos Plantier Jr <junior.plantier@gmail.com>
2020-01-25 18:59:22 -0200

    Produto e associação muito para muitos com a Categoria

--- src/main/java/com/plantier/pedidos/PlantierPedidosApplication.java
+++ src/main/java/com/plantier/pedidos/PlantierPedidosApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.plantier.pedidos.domain.Categoria;
+import com.plantier.pedidos.domain.Produto;
 import com.plantier.pedidos.repositories.CategoriaRepository;
+import com.plantier.pedidos.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class PlantierPedidosApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(PlantierPedidosApplication.class, args);
@@ -26,7 +31,20 @@ public class PlantierPedidosApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		// Adiciona as categorias a lista de produtos 
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		// Os produtos precisam conhecer sua categoria
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 
 }
