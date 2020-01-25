ef741d7c2fa36c08aca7c7dbaedf56258a53eed7
Carlos Plantier Jr <junior.plantier@gmail.com>
2020-01-25 20:28:12 -0200

    Criação das entidades: Estado e Cidade

--- src/main/java/com/plantier/pedidos/domain/Categoria.java
+++ src/main/java/com/plantier/pedidos/domain/Categoria.java
@@ -10,7 +10,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 public class Categoria implements Serializable {
@@ -21,8 +21,8 @@ public class Categoria implements Serializable {
 	private Integer id;
 	private String nome;
 
-	//@JsonManagedReference
-	@JsonIgnore
+	@JsonManagedReference
+	//@JsonIgnore
 	@ManyToMany(mappedBy="categorias")
 	private List<Produto> produtos = new ArrayList<>();
 	
--- src/main/java/com/plantier/pedidos/domain/Cidade.java
+++ src/main/java/com/plantier/pedidos/domain/Cidade.java
@@ -1,45 +1,35 @@
 package com.plantier.pedidos.domain;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import javax.persistence.ManyToOne;
 
 @Entity
-public class Produto implements Serializable {
+public class Cidade implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
-	private Double preco;
-
-	@JsonBackReference
-	@ManyToMany
-	@JoinTable(name="PRODUTO_CATEGORIA",
-			   joinColumns =        @JoinColumn(name="produto_id"),
-			   inverseJoinColumns = @JoinColumn(name="categoria_id")
-			  )
-	private List<Categoria> categorias = new ArrayList<>();
 	
-	public Produto() {
-	}
+	@ManyToOne
+	@JoinColumn(name="estado_id")
+	private Estado estado;
 
-	public Produto(Integer id, String nome, Double preco) {
+	public Cidade() {
+	}
+	
+	public Cidade(Integer id, String nome, Estado estado) {
 		super();
 		this.id = id;
 		this.nome = nome;
-		this.preco = preco;
+		this.estado = estado;
 	}
 
 	public Integer getId() {
@@ -58,20 +48,12 @@ public class Produto implements Serializable {
 		this.nome = nome;
 	}
 
-	public Double getPreco() {
-		return preco;
+	public Estado getEstado() {
+		return estado;
 	}
 
-	public void setPreco(Double preco) {
-		this.preco = preco;
-	}
-
-	public List<Categoria> getCategorias() {
-		return categorias;
-	}
-
-	public void setCategorias(List<Categoria> categorias) {
-		this.categorias = categorias;
+	public void setEstado(Estado estado) {
+		this.estado = estado;
 	}
 
 	@Override
@@ -90,7 +72,7 @@ public class Produto implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Produto other = (Produto) obj;
+		Cidade other = (Cidade) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -98,5 +80,4 @@ public class Produto implements Serializable {
 			return false;
 		return true;
 	}
-	
 }
