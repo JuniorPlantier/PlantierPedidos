31476776d0d67d2d1469b499fce617aca0b20116
Carlos Plantier Jr <junior.plantier@gmail.com>
2020-01-25 12:46:42 -0200

    Projeto criado

---
+++ src/main/java/com/plantier/pedidos/PlantierPedidosApplication.java
@@ -0,0 +1,13 @@
+package com.plantier.pedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantierPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantierPedidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plantier/pedidos/PlantierPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plantier.pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantierPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
