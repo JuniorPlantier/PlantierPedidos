9fc481b03f10c627ab4fe23a9da9b2b4bac4a32c
Carlos Plantier Jr <junior.plantier@gmail.com>
2020-01-25 23:31:29 -0200

    Pedido, Pagamento(e subclasses) e EstadoPagamento

--- src/main/java/com/plantier/pedidos/PlantierPedidosApplication.java
+++ src/main/java/com/plantier/pedidos/PlantierPedidosApplication.java
@@ -1,5 +1,6 @@
 package com.plantier.pedidos;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.plantier.pedidos.domain.Cidade;
 import com.plantier.pedidos.domain.Cliente;
 import com.plantier.pedidos.domain.Endereco;
 import com.plantier.pedidos.domain.Estado;
+import com.plantier.pedidos.domain.Pagamento;
+import com.plantier.pedidos.domain.PagamentoComBoleto;
+import com.plantier.pedidos.domain.PagamentoComCartao;
+import com.plantier.pedidos.domain.Pedido;
 import com.plantier.pedidos.domain.Produto;
+import com.plantier.pedidos.domain.enums.EstadoPagamento;
 import com.plantier.pedidos.domain.enums.TipoCliente;
 import com.plantier.pedidos.repositories.CategoriaRepository;
 import com.plantier.pedidos.repositories.CidadeRepository;
 import com.plantier.pedidos.repositories.ClienteRepository;
 import com.plantier.pedidos.repositories.EnderecoRepository;
 import com.plantier.pedidos.repositories.EstadoRepository;
+import com.plantier.pedidos.repositories.PagamentoRepository;
+import com.plantier.pedidos.repositories.PedidoRepository;
 import com.plantier.pedidos.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -41,6 +49,12 @@ public class PlantierPedidosApplication implements CommandLineRunner {
 	
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(PlantierPedidosApplication.class, args);
@@ -90,9 +104,25 @@ public class PlantierPedidosApplication implements CommandLineRunner {
 		
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
 		
-		
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		
+		// ###########
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm"); 
+		
+		Pedido ped1 = new Pedido(null, sdf.parse("25/01/2020 23:18"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("02/03/2020 18:20"), cli1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("04/02/2020 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 		
 	}	
 
