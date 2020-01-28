14cc4b876704f5b1f62b797886a1af0142358c7d
Carlos Plantier Jr <junior.plantier@gmail.com>
2020-01-28 14:03:25 -0200

    Correções: update Categoria Service e construtor de Pagamento

--- src/main/java/com/plantier/pedidos/services/CategoriaService.java
+++ src/main/java/com/plantier/pedidos/services/CategoriaService.java
@@ -35,7 +35,8 @@ public class CategoriaService {
 	}
 	
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
 		return repo.save(obj);
 	}
 	
@@ -61,5 +62,9 @@ public class CategoriaService {
 	// a partir de um DTO, é construído um objeto Categoria.
 	public Categoria castDto(CategoriaDTO objDto) {
 		return new Categoria(objDto.getId(), objDto.getNome());
+	}
+	
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
 	}
 }
