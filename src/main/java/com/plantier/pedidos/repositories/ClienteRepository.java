c8d9308728bc9f92a1ebcdd1f63c4a22adb788e7
Carlos Plantier Jr <junior.plantier@gmail.com>
2020-01-31 17:09:21 -0200

    validação customizada, email único

--- src/main/java/com/plantier/pedidos/repositories/ClienteRepository.java
+++ src/main/java/com/plantier/pedidos/repositories/ClienteRepository.java
@@ -1,11 +1,18 @@
 package com.plantier.pedidos.repositories;
 
+
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.plantier.pedidos.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+	
+	// Recurso do springdata que usa um padrão de nomes. 
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
 	
 }
--- src/main/java/com/plantier/pedidos/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/plantier/pedidos/services/validation/ClienteInsertValidator.java
@@ -6,13 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.plantier.pedidos.domain.Cliente;
 import com.plantier.pedidos.domain.enums.TipoCliente;
 import com.plantier.pedidos.dto.ClienteNewDTO;
+import com.plantier.pedidos.repositories.ClienteRepository;
 import com.plantier.pedidos.resources.exceptions.FieldMessage;
 import com.plantier.pedidos.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
 
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -28,6 +35,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		
 		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux == null) {
+			list.add(new FieldMessage("email", "Email já existe"));
 		}
 		
 		/*
