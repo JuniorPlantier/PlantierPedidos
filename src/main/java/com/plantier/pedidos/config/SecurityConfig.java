1fddd06aa676141f26227c0562120103546a15f6
Carlos Plantier Jr <junior.plantier@gmail.com>
2020-03-28 13:56:14 -0300

    Implementando Autorização

--- src/main/java/com/plantier/pedidos/config/SecurityConfig.java
+++ src/main/java/com/plantier/pedidos/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.plantier.pedidos.security.JWTAuthenticationFilter;
+import com.plantier.pedidos.security.JWTAuthorizationFilter;
 import com.plantier.pedidos.security.JWTUtil;
 
 @Configuration
@@ -52,6 +53,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated();
 		
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
--- src/main/java/com/plantier/pedidos/security/JWTUtil.java
+++ src/main/java/com/plantier/pedidos/security/JWTUtil.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
@@ -24,4 +25,40 @@ public class JWTUtil {
 				.signWith(SignatureAlgorithm.HS512, secret.getBytes())
 				.compact();
 	}
+	
+	public boolean tokenValido(String token) {
+		// Claims é um tipo do JWT que armazena as reivindicações do token.
+		Claims claims = getClaims(token);
+		if(claims != null) {
+			String username = claims.getSubject();
+			Date expirationDate = claims.getExpiration();
+			Date now = new Date(System.currentTimeMillis());
+			
+			if (username != null && expirationDate != null && now.before(expirationDate)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public String getUsername(String token) {
+		Claims claims = getClaims(token);
+		if(claims != null) {
+			return claims.getSubject();
+		}
+		return null;
+	}
+	
+	private Claims getClaims(String token) {
+		try {
+			//obtém os clamis a partir de um token
+			return Jwts.parser().setSigningKey(secret.getBytes()).parseClaimsJws(token).getBody();
+		} catch(Exception e) {
+			// se o token for inválido ou der algum problema, retorno nulo.
+			return null;
+		}
+		
+	}
+	
+	
 }
