81646044fd5ca892a6722d7e3c6b75a236812378
Carlos Plantier Jr <junior.plantier@gmail.com>
2020-03-30 01:31:53 -0300

    Salvando o primeiro arquivo no s3

--- src/main/java/com/plantier/pedidos/PlantierPedidosApplication.java
+++ src/main/java/com/plantier/pedidos/PlantierPedidosApplication.java
@@ -1,11 +1,17 @@
 package com.plantier.pedidos;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.plantier.pedidos.services.S3Service;
+
 @SpringBootApplication
 public class PlantierPedidosApplication implements CommandLineRunner {
+	
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(PlantierPedidosApplication.class, args);
@@ -13,6 +19,8 @@ public class PlantierPedidosApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		// Testa Upload do s3Amanon
+		s3Service.uploadFile("C:\\Users\\junior\\Desktop\\Pedidos\\fotos\\curioso.jpg");
 	}	
 
 }
