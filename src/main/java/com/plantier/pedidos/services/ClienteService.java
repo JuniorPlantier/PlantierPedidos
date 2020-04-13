4762a8382f0501328cae98f986913170c57bb13c
Carlos Plantier Jr <junior.plantier@gmail.com>
2020-04-13 17:42:56 -0300

    Ajustando o tamanho da imagem

--- src/main/java/com/plantier/pedidos/services/ClienteService.java
+++ src/main/java/com/plantier/pedidos/services/ClienteService.java
@@ -51,6 +51,9 @@ public class ClienteService {
 	@Value("${img.prefix.client.profile}")
 	private String prefix;
 	
+	@Value("${img.profile.size}")
+	private Integer size;
+	
 	public Cliente find(Integer id){
 		UserSS user = UserService.authenticated();
 		if (user == null || !user.hasRole(Perfil.ADMIN) &&  !id.equals(user.getId())) {
@@ -129,6 +132,8 @@ public class ClienteService {
 		}
 
 		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+		jpgImage = imageService.cropSquare(jpgImage);
+		jpgImage = imageService.resize(jpgImage, size);
 		String fileName = prefix + user.getId() + ".jpg";
 		
 		return s3Service.uploadFile(fileName, imageService.getInputStream(jpgImage, "jpg"), "image");
