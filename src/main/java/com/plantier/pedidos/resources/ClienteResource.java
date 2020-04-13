29379761fea86581d7cee249fec085380553b0de
Carlos Plantier Jr <junior.plantier@gmail.com>
2020-04-13 11:54:07 -0300

    Enviando imagem via endpoint

--- src/main/java/com/plantier/pedidos/resources/ClienteResource.java
+++ src/main/java/com/plantier/pedidos/resources/ClienteResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.plantier.pedidos.domain.Cliente;
@@ -85,5 +86,10 @@ public class ClienteResource {
 		return ResponseEntity.created(uri).build();
 		
 	}
-		
+	
+	@RequestMapping(value="/picture", method=RequestMethod.POST)
+	public ResponseEntity<Void> uploadProfilePicture(@RequestParam(name="file") MultipartFile file) {
+		URI uri = service.uploadProfilePicture(file);
+		return ResponseEntity.created(uri).build();
+	}
 }
--- src/main/java/com/plantier/pedidos/services/ClienteService.java
+++ src/main/java/com/plantier/pedidos/services/ClienteService.java
@@ -1,5 +1,6 @@
 package com.plantier.pedidos.services;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,6 +12,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.plantier.pedidos.domain.Cidade;
 import com.plantier.pedidos.domain.Cliente;
@@ -37,6 +39,9 @@ public class ClienteService {
 	
 	@Autowired
 	private BCryptPasswordEncoder pe;
+	
+	@Autowired
+	private S3Service s3service;
 	
 	public Cliente find(Integer id){
 		UserSS user = UserService.authenticated();
@@ -107,5 +112,9 @@ public class ClienteService {
 			cli.getTelefones().add(objDTO.getTelefone3());
 		}
 		return cli;
+	}
+	
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+		return s3service.uploadFile(multipartFile);
 	}
 }
